72af89c03ecb5a1077b11b4bddbb1c3438739812
pat mccarthy <152814651+wicklowtown@users.noreply.github.com>
2023-12-16 08:00:05 +0000

    V1.4
    Issues loading submarine sprite is fixed, as well as problems loading audio.
    A new .wav file was added to add more underwater ambience.
    'Spacebar' to play music removed - music automatically plays upon starting the game.

--- main.c
+++ main.c
@@ -3,71 +3,55 @@
 
 #include "raylib.h"
 
+//Main set-up section
+
 int main() {
     const int screenWidth = 1280;
     const int screenHeight = 720;
-    InitWindow(1280, 720, "Fenian Ram 3000");
+    InitWindow(screenWidth, screenHeight, "Fenian Ram 3000");
+
     SetTargetFPS(60);
 
-    // Using guidance from lecture 3, I added music.
+//Music set-up section
+//This was very difficult to set up. I tried removing the feature of pressing spacebar to start the sound,
+// as shown in Lecture 3, because I wanted the music files to play automatically throughout.
+// I found solutions by using the ‘LoadMusicStream’ and ‘PlayMusicStream’ features.
+// The resource I used to find this solution was:
+// https://www.raylib.com/examples/audio/loader.html?name=audio_music_stream
+
     InitAudioDevice();
-    Sound sound = LoadSound("music.wav");
-    int volume = 50;
-    SetSoundVolume(sound, volume/100.0f);
+    Music music1 = LoadMusicStream("music.wav");
+    Music music2 = LoadMusicStream("Underwatersounds.wav");
 
+    PlayMusicStream(music1);
+    PlayMusicStream(music2);
 
-    // Using guidance from Lecture 3, I began to load textures, add vectors and more.
-    // The first image I added was a test for the playable sprite - the submarine.
+    // Submarine Sprite set-up section
+    // I increased the moveSpeed as shown in the lecture from 5.0f to 7.0f.
     Texture2D submarine1 = LoadTexture("submarine.png");
-Vector2 scale = {0.5f/0.5f};
+    Vector2 position = {(float) screenWidth / 2 - submarine1.width / 2,
+                        (float) screenHeight / 2 - submarine1.height / 2};
+    Vector2 scale = {0.5f, 0.5f};
+    float moveSpeed = 7.0f;
 
-     float rotation = 0.0f;
-     Color tint = WHITE;
+    // Using further guidance from Lecture 3, I added movement inputs to the sprite.
 
-    Vector2 position = {(screenWidth - (submarine1.width* scale.x))/2, (screenHeight- (submarine1.height * scale.y))/2};
-const float moveSpeed = 5.0f;
+    // Submarine Movement
+    if (IsKeyDown(KEY_RIGHT)) position.x += moveSpeed;
+    if (IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
+    if (IsKeyDown(KEY_UP)) position.y -= moveSpeed;
+    if (IsKeyDown(KEY_DOWN)) position.y += moveSpeed;
 
-    // The Game Loop
-    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
+    UpdateMusicStream(music1);
+    UpdateMusicStream(music2);
 
-// Using further guidance from Lecture 3, I added movement inputs to the sprite.
-        if (IsKeyDown(KEY_RIGHT)) position.x += moveSpeed;
-        if (IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
-        if (IsKeyDown(KEY_UP)) position.y -= moveSpeed;
-        if (IsKeyDown(KEY_DOWN)) position.y += moveSpeed;
+    BeginDrawing();
+    ClearBackground(BLUE);
+    DrawTextureEx(submarine1, position, 0.0f, 0.5f, BLACK);
+    // Here goes all the Game Logic
+    EndDrawing();
 
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) rotation -= 10.0f;
-        if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) rotation += 10.0f;}
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-        if (IsKeyPressed(KEY_SPACE)) {
-            PlaySound(LoadSound("music.wav"));
-        }
-        if (IsKeyPressed(KEY_UP) && volume < 100) {
-            volume += 10;
-            SetSoundVolume(sound, volume / 100.0f);
-        }
-        if (IsKeyPressed(KEY_DOWN) && volume < 100) {
-            volume -= 10;
-            SetSoundVolume(sound, volume / 100.0f);
-
-            Font customFont = LoadFont("grasping.ttf");
-            Font customFont2 = LoadFont("COMICATE.TTF");
-            Font customFont3 = LoadFont("Outwrite.ttf");
-
-
-            Vector2 textPosition = {10, screenHeight / 1 - 45};
-            int textSize = 45;
-            Color textColor = LIME;
-
-            // Here goes all the Game Logic
-
-            // teardown Canvas
-            EndDrawing();
-        }
-        UnloadTexture(submarine1);
-        CloseWindow();
-        return 0;
-    }
+    UnloadTexture(submarine1);
+    CloseWindow();
+    return 0;
+}
